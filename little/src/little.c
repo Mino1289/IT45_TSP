d20e2fd024d88c9785a55925c1f4bdff586f8886
Mino1289 <kretzvictor@orange.fr>
2024-06-19 14:56:35 +0200

    Final Commit

--- little/src/little.c
+++ little/src/little.c
@@ -12,7 +12,7 @@
 #include <time.h>
 #include <stdbool.h>
 
-// #define LITTLEPLUS
+#define LITTLEPLUS
 #ifdef LITTLEPLUS
 #warning "LITTLEPLUS is defined"
 #else
@@ -20,7 +20,7 @@
 #endif
 
 #define BIG_VALUE 1e9
-#define NBR_TOWNS 6
+#define NBR_TOWNS 15
 
  /* Distance matrix */
 float dist[NBR_TOWNS][NBR_TOWNS];
@@ -31,7 +31,6 @@ int next_town[NBR_TOWNS];
 /* no comment */
 int best_solution[NBR_TOWNS];
 float best_eval = -1.0;
-int nbit = 0;
 
 
 /**
@@ -54,6 +53,8 @@ int nbit = 0;
  * KroA100 :
  * 100 towns : (26856.389) -> Best solution ?
  */
+
+// Berlin52
 float coord[NBR_TOWNS][2] =
 {
     {565.0, 575.0},
@@ -66,6 +67,48 @@ float coord[NBR_TOWNS][2] =
     {525.0, 1000.0},
     {580.0, 1175.0},
     {650.0, 1130.0},
+    {1605.0, 620.0}, 
+    {1220.0, 580.0},
+    {1465.0, 200.0},
+    {1530.0, 5.0},
+    {845.0, 680.0},
+    {725.0, 370.0},
+    {145.0, 665.0},
+    {415.0, 635.0},
+    {510.0, 875.0},  
+    {560.0, 365.0},
+    {300.0, 465.0},
+    {520.0, 585.0},
+    {480.0, 415.0},
+    {835.0, 625.0},
+    {975.0, 580.0},
+    {1215.0, 245.0},
+    {1320.0, 315.0},
+    {1250.0, 400.0},
+    {660.0, 180.0},
+    {410.0, 250.0},
+    {420.0, 555.0},
+    {575.0, 665.0},
+    {1150.0, 1160.0},
+    {700.0, 580.0},
+    {685.0, 595.0},
+    {685.0, 610.0},
+    {770.0, 610.0},
+    {795.0, 645.0},
+    {720.0, 635.0},
+    {760.0, 650.0},
+    {475.0, 960.0},
+    {95.0, 260.0},
+    {875.0, 920.0},
+    {700.0, 500.0},
+    {555.0, 815.0},
+    {830.0, 485.0},
+    {1170.0, 65.0},
+    {830.0, 610.0},
+    {605.0, 625.0},
+    {595.0, 360.0},
+    {1340.0, 725.0},
+    {1740.0, 245.0},
 };
 
 
@@ -248,7 +291,6 @@ bool creates_subtour(int start, int end) {
  *  Little Algorithm
  */
 void little_algorithm(float d0[NBR_TOWNS][NBR_TOWNS], int iteration, float eval_node_parent) {
-    nbit++;
     if (iteration == NBR_TOWNS) {
         build_solution();
         return;
@@ -301,8 +343,9 @@ void little_algorithm(float d0[NBR_TOWNS][NBR_TOWNS], int iteration, float eval_
 
 
     /* Cut : stop the exploration of this node */
-    if (best_eval >= 0 && eval_node_child >= best_eval)
+    if (best_eval >= 0 && eval_node_child >= best_eval) {
         return;
+    }
 
     /* row and column of the zero with the max penalty */
     int izero = -1, jzero = -1;
@@ -313,7 +356,7 @@ void little_algorithm(float d0[NBR_TOWNS][NBR_TOWNS], int iteration, float eval_
         for (j = 0; j < NBR_TOWNS; j++) {
             if (d[i][j] == 0) {
 #ifdef LITTLEPLUS
-                if (!creates_subtour(j, i)) {
+                if (!creates_subtour(i, j)) {
 #endif
                     float min_row = -1;
                     float min_col = -1;
@@ -340,6 +383,8 @@ void little_algorithm(float d0[NBR_TOWNS][NBR_TOWNS], int iteration, float eval_
                         jzero = j;
                     }
 #ifdef LITTLEPLUS
+                } else {
+                    d[i][j] = -1;
                 }
 #endif
             }
@@ -367,6 +412,7 @@ void little_algorithm(float d0[NBR_TOWNS][NBR_TOWNS], int iteration, float eval_
     /* Do the modification on a copy of the distance matrix */
     memcpy(d2, d, NBR_TOWNS * NBR_TOWNS * sizeof(float));
 
+    // next_town[izero] = -1;
     d2[izero][jzero] = -1;
 
     little_algorithm(d2, iteration, eval_node_child);
@@ -406,6 +452,5 @@ int main(int argc, char* argv[]) {
     print_solution(best_solution, best_eval);
 
     printf("Time: %f seconds\n", difftime(end, start));
-    printf("Number of iterations: %d\n", nbit);
     return 0;
 }
